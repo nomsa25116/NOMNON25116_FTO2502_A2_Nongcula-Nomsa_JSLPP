552c534ac920551b04f393bfaa8003c0d3b1726d
nomsa25116 <ntnongcula@gmail.com>
2025-05-30 11:38:15 +0200

    Remove theme.js and add modal open/close logic and theme toggle

--- scripts.js/theme.js
+++
@@ -1,8 +0,0 @@
-// Check localStorage for mode on load
-const toggle = document.getElementById("themeToggle");
-const body = document.body;
-
-toggle.addEventListener("change", () => {
-  body.classList.toggle("dark");
-  body.classList.toggle("light");
-});
